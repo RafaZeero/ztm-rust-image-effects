dc4586d06e778203da410ab561d34b93dcea2f34
Rafael Lima <rafaa99@gmail.com>
2022-06-27 16:15:59 -0300

    add rotate 180degree img & both rotation and grayscaling options/logic

--- public/main.js
+++ public/main.js
@@ -10,7 +10,19 @@ async function init() {
   }
 
   // select element ID 'upload' at the DOM
-  const input = document.getElementById('upload')
+  const input_gray = document.getElementById('upload_gray')
+  const input_upsidedown = document.getElementById('upload_upsidedown')
+  const input_both = document.getElementById('upload_both')
+  // spinner logic
+  const spinner = document.getElementById('spinner')
+
+  function addSpinner() {
+    spinner.classList.remove('hidden')
+  }
+
+  function removeSpinner() {
+    spinner.classList.add('hidden')
+  }
 
   // instantiate the FileReader to read the uploaded file
   const fileRead = new FileReader()
@@ -20,16 +32,77 @@ async function init() {
   //// fileRead.result ~> Returns a string
 
   // the property onloadend is used only when the file image is completely uploaded
-  fileRead.onloadend = () => {
-    let base64 = fileRead.result.replace(
-      /^data:image\/(png|jpeg|jpg);base64,/,
-      ''
-    )
-    rustApp.grayscale(base64)
+  // make file grayscale
+  function toGrayscale() {
+    fileRead.onloadend = () => {
+      let base64 = fileRead.result.replace(
+        /^data:image\/(png|jpeg|jpg);base64,/,
+        ''
+      )
+      let img_data_url = rustApp.grayscale(base64)
+
+      document.getElementById('new-img').setAttribute('src', img_data_url)
+      removeSpinner()
+    }
+  }
+  //make file rotate 180
+  function rotate180deg() {
+    fileRead.onloadend = () => {
+      let base64 = fileRead.result.replace(
+        /^data:image\/(png|jpeg|jpg);base64,/,
+        ''
+      )
+      let img_data_url = rustApp.rotate180(base64)
+      document.getElementById('new-img').setAttribute('src', img_data_url)
+      removeSpinner()
+    }
+  }
+  // make file grayscale AND rotate 180
+  function rotate180AndToGrayscale() {
+    fileRead.onloadend = () => {
+      let base64 = fileRead.result.replace(
+        /^data:image\/(png|jpeg|jpg);base64,/,
+        ''
+      )
+      let img_data_url = rustApp.grayscale(base64)
+
+      let base64_second = img_data_url.replace(
+        /^data:image\/(png|jpeg|jpg);base64,/,
+        ''
+      )
+      let img_data_url_second = rustApp.rotate180(base64_second)
+
+      document
+        .getElementById('new-img')
+        .setAttribute('src', img_data_url_second)
+      removeSpinner()
+    }
   }
 
-  input.addEventListener('change', () => {
-    fileRead.readAsDataURL(input.files[0])
+  // logic to send file and start changing its colors
+  // to grayscale
+  input_gray.addEventListener('change', () => {
+    addSpinner()
+    setTimeout(() => {
+      fileRead.readAsDataURL(input_gray.files[0])
+      toGrayscale()
+    }, 1000)
+  })
+  // to invert colors
+  input_upsidedown.addEventListener('change', () => {
+    addSpinner()
+    setTimeout(() => {
+      fileRead.readAsDataURL(input_upsidedown.files[0])
+      rotate180deg()
+    }, 1000)
+  })
+  // to invert colors AND grayscale
+  input_both.addEventListener('change', () => {
+    addSpinner()
+    setTimeout(() => {
+      fileRead.readAsDataURL(input_both.files[0])
+      rotate180AndToGrayscale()
+    }, 1000)
   })
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,67 @@
-use base64::decode;
+use base64::{decode, encode};
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     //convert a string into a javascript value
     // log(&encoded_file.into());
     log(&"Grayscale called.".into());
 
+    // decoding the file from base64 to string
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded.".into());
 
+    // loading an image from the memory
     let mut img = load_from_memory(&base64_to_vector).unwrap();
     log(&"Image loaded.".into());
 
+    // apply grayscale to the image
     img = img.grayscale();
     log(&"Grayscale effect applied.".into());
+
+    // transform image into PNG format
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written.".into());
+
+    // encoding image to url format to send it back to user
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    // return the encoded image string
+    data_url
+}
+
+#[wasm_bindgen]
+pub fn rotate180(encoded_file: &str) -> String {
+    //convert a string into a javascript value
+    // log(&encoded_file.into());
+    log(&"Rotate 180º called.".into());
+
+    // decoding the file from base64 to string
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded.".into());
+
+    // loading an image from the memory
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded.".into());
+
+    // apply rotation 180 to the image
+    img = img.rotate180();
+    log(&"Rotate effect applied.".into());
+
+    // transform image into PNG format
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written.".into());
+
+    // encoding image to url format to send it back to user
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    // return the encoded image string
+    data_url
 }
